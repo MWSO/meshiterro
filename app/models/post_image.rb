b46240ae8a7589b6f9714d6ac024589c3eb79037
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2022-12-16 10:31:00 +0000

    comment and iine

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,6 +3,10 @@ class PostImage < ApplicationRecord
 
   belongs_to :user
 
+  has_many :post_comments, dependent: :destroy
+
+  has_many :favorites, dependent: :destroy
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join("app/assets/images/no_image.jpg")
@@ -10,4 +14,9 @@ class PostImage < ApplicationRecord
     end
       "no_image.jpg"
   end
+
+def favorited_by?(user)
+  favorites.exists?(user_id: user.id)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
- resources :users, only: [:show, :edit]
- resources :post_images, only: [:new, :show, :index, :create, :destroy]
+ resources :users, only: [:show, :edit, :update]
+ resources :post_images, only: [:new, :show, :index, :create, :destroy] do
+  resources :post_comments, only: [:create, :destroy]
+  resource :favorites, only: [:create, :destroy]
+ end
+
   devise_for :users
 root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get "about" => "homes#about", as: "about"
+
 end
